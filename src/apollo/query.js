65ec06272aaed3e0f3076ef1dcd36c85e2f691e2
starlord <starlord@xhiftcorp.com>
2019-04-15 16:26:02 +0900

    comment

--- src/apollo/query.js
+++ src/apollo/query.js
@@ -1,6 +1,8 @@
 import gql from "graphql-tag";
 import { NOTE_FRAHMENT } from "./fragment";
 
+//client는 없을경우에는 무조건 외부 http api로 인식하고 
+// @client가 붙었을때는 가로채서 내부 로컬에서 돌린다.(cache)
 export const GET_NOTES = gql`
      {
           notes @client {
